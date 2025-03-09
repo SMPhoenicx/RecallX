8b686951377a5eee15fdf476ae4d5688c17bd78f
Suman <sumanmuppavarapu@gmail.com>
2025-03-09 17:33:21 -0500

    Initial Commit

---
+++ RecallX/RecallXApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecallXApp.swift
+//  RecallX
+//
+//  Created by Suman Muppavarapu on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct RecallXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecallXTests/RecallXTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecallXTests.swift
+//  RecallXTests
+//
+//  Created by Suman Muppavarapu on 3/9/25.
+//
+
+import Testing
+@testable import RecallX
+
+struct RecallXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
